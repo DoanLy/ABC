9a54a3f28dc4d2cf679ed7a302b1418de78f383d
DoanLy <lydoan.king@gmail.com>
2018-06-15 23:34:27 +0700

    make it better

--- TH_Loaisach.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include_once("ketnoi.php");
-$ploai= taobang ("select * from loaisach");
-while($loai=mysqli_fetch_array($ploai))
-	
-	echo "<a href='#' onClick='HienThiSach(".$loai["Maloai"].")';> ".$loai["Tenloai"]."</a><br>";
-
-?>
